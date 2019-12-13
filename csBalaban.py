31d8224df085db0a8676bbe9f92c8d5d1a8b4f4f
popolinka <55590370+popolinka@users.noreply.github.com>
2019-12-13 16:32:18 +0100

    ref

--- csBalaban.py
+++ csBalaban.py
@@ -2,11 +2,21 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
+
+whitelist = [
+    'p' , 'h4', 'div' , 'h3'
+]
+blackList = [
+    '\n', ' ', 'E-posta', ' ', 'Telefon', 'Mesaj', 'İsim'
+]
+
 def getwod(url):
     url = url
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    wod = soup.findAll("div", {"class" : "post-body entry-content"})
+    # wod = soup.findAll("div", {"class" : "post-body entry-content"})
+    wod = soup.findAll("span", {"span style": "font-size: large;"})
+
     # liste = [t for t in soup.find_all(text=True) if t.parent.name in whitelist and t not in blackList]
 
     return wod
