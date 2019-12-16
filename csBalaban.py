3acaebaad0a655937b3fb657ca96e2f42fc4540a
popolinka <55590370+popolinka@users.noreply.github.com>
2019-12-16 09:24:34 +0100

    scraping a single page is done :)

--- csBalaban.py
+++ csBalaban.py
@@ -2,23 +2,29 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
-
 whitelist = [
-    'p' , 'h4', 'div' , 'h3'
+    'p', 'h4', 'div', 'h3'
 ]
 blackList = [
     '\n', ' ', 'E-posta', ' ', 'Telefon', 'Mesaj', 'İsim'
 ]
 
+
 def getwod(url):
+    listem = []
     url = url
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    # wod = soup.findAll("div", {"class" : "post-body entry-content"})
-    wod = soup.findAll("span", {"span style": "font-size: large;"})
+    wods = soup.findAll("div", {"class" : "post-body entry-content"})
+    for wod in wods:
+        listem.append(wod.text.replace('\n', '')
+)
+    # wod = soup.findAll("span", {"span style": "font-size: large;"})
 
     # liste = [t for t in soup.find_all(text=True) if t.parent.name in whitelist and t not in blackList]
+    # liste = [t for t in soup.findAll("div", {"class" : "post-body entry-content"}) if t.parent.name in whitelist and t not in blackList]
 
-    return wod
+    return listem
+
 
 print(getwod("http://crossfitbalabanlevent.blogspot.com/2019/10/"))
