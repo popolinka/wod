4e6507175b8937d97895738e65c1ba666a407221
popolinka <55590370+popolinka@users.noreply.github.com>
2019-12-16 11:15:36 +0100

    serious TODO

--- csBalaban.py
+++ csBalaban.py
@@ -12,19 +12,38 @@ blackList = [
 
 def getwod(url):
     listem = []
-    url = url
+    # url = url
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    wods = soup.findAll("div", {"class" : "post-body entry-content"})
+    wods = soup.findAll("div", {"class": "post-body entry-content"})
     for wod in wods:
-        listem.append(wod.text.replace('\n', '')
-)
-    # wod = soup.findAll("span", {"span style": "font-size: large;"})
-
-    # liste = [t for t in soup.find_all(text=True) if t.parent.name in whitelist and t not in blackList]
-    # liste = [t for t in soup.findAll("div", {"class" : "post-body entry-content"}) if t.parent.name in whitelist and t not in blackList]
+        listem.append(wod.text.replace('\n', ''))
 
     return listem
 
 
-print(getwod("http://crossfitbalabanlevent.blogspot.com/2019/10/"))
+url = "http://crossfitbalabanlevent.blogspot.com/"
+
+workouts = []
+# years = [2014, 2015, 2016, 2017, 2018, 2019, 2020]
+years = [2014, 2015]
+months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+# updated Python does not support format 01 02 etc.
+# TODO: when the if statement is triggered, the output is simply an emtpy list
+
+for y in years:
+    for m in months:
+        url = url + str(y) + "/" + m + "/"
+        # workouts.append(getwod(url))
+        if getwod(url):
+            workouts.append(getwod(url))
+
+        # if requests.get(url).status_code == 200:
+        #     workouts.append(getwod(url))
+        # else:
+        #     pass
+    time.sleep(1)
+
+# print(getwod("http://crossfitbalabanlevent.blogspot.com/2019/10/"))
+
+print(workouts)
