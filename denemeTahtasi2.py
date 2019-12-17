ec16fce2f638e455bcc3934350bba26e6973d6f8
popolinka <55590370+popolinka@users.noreply.github.com>
2019-12-17 17:18:31 +0100

    Updating
    Crawling works, need refactoring, and formatting

--- denemeTahtasi2.py
+++ denemeTahtasi2.py
@@ -28,29 +28,44 @@ print(getwod("http://crossfitbalabanlevent.blogspot.com/2014/10")[1])
 
 # print(len(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01")))
 print(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01") == None)
-#print(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01")[1] != [])
-#print(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01")[1])
+print(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01"))
 
+# print(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01")[1] != [])
+# print(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01")[1])
 
 
 link = "http://crossfitbalabanlevent.blogspot.com/"
 
 workouts = []
-years = [2014, 2015]
-months = ["09", "10", "11", "12"]
+years = [2015, 2016]
+months = ["01", "02"]
 # updated Python does not support format 01 02 etc.
 # TODO: when the if statement is triggered, the output is simply an emtpy list
 
 for y in years:
+    link = link + str(y) + "/"
     for m in months:
-        link = link + str(y) + "/" + m + "/"
-        if getwod(link) is None:
-            pass
-        else:
+        link = link + m + "/"
+        if getwod(link) is not None:
             workouts.append(getwod(link)[1])
+        # else:
 
     time.sleep(1)
 
 # print(getwod("http://crossfitbalabanlevent.blogspot.com/2019/10/"))
 
-print(workouts)
+f = open('BalabanCS_wods.txt', 'w+')
+
+for x in workouts[0]:
+    f.write('%s\n' % x)  # splitting each wod/item into a seperate row
+
+    # TODO wodsCombinedComaSplit.append(x[:-1]) # removing the last /n line
+
+f.close()
+
+# print(workouts)
+# print(type(workouts))
+# print(len(workouts))
+# print(len(workouts[0]))
+
+
