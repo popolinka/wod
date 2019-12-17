3d207cc2eeb28e2ba19a90d9052e3ac0b5a91128
popolinka <55590370+popolinka@users.noreply.github.com>
2019-12-17 14:06:49 +0100

    A bit of an improvement

--- denemeTahtasi2.py
+++ denemeTahtasi2.py
@@ -2,13 +2,6 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
-whitelist = [
-    'p', 'h4', 'div', 'h3'
-]
-blackList = [
-    '\n', ' ', 'E-posta', ' ', 'Telefon', 'Mesaj', 'İsim'
-]
-
 
 def getwod(url):
     listem = []
@@ -16,32 +9,46 @@ def getwod(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     wods = soup.findAll("div", {"class": "post-body entry-content"})
-    for wod in wods:
-        listem.append(wod.text.replace('\n', ''))
 
-    return listem
+    if wods:  # TODO: derdin ne abi, aslinda wods != [] in simple hali o
+        for wod in wods:
+            if len(wod) > 0:
+                listem.append(wod.text.replace('\n', ''))
+            else:
+                pass
+    if wods:
+        return [wods, listem]
 
 
-url = "http://crossfitbalabanlevent.blogspot.com/"
+# print(len(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01")))
+# print(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01"))
+
+print(len(getwod("http://crossfitbalabanlevent.blogspot.com/2014/10")))
+print(getwod("http://crossfitbalabanlevent.blogspot.com/2014/10")[1])
+
+# print(len(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01")))
+print(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01") == None)
+#print(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01")[1] != [])
+#print(getwod("http://crossfitbalabanlevent.blogspot.com/2014/01")[1])
+
+
+
+link = "http://crossfitbalabanlevent.blogspot.com/"
 
 workouts = []
-# years = [2014, 2015, 2016, 2017, 2018, 2019, 2020]
 years = [2014, 2015]
-months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+months = ["09", "10", "11", "12"]
 # updated Python does not support format 01 02 etc.
 # TODO: when the if statement is triggered, the output is simply an emtpy list
 
 for y in years:
     for m in months:
-        url = url + str(y) + "/" + m + "/"
-        # workouts.append(getwod(url))
-        if getwod(url):
-            workouts.append(getwod(url))
+        link = link + str(y) + "/" + m + "/"
+        if getwod(link) is None:
+            pass
+        else:
+            workouts.append(getwod(link)[1])
 
-        # if requests.get(url).status_code == 200:
-        #     workouts.append(getwod(url))
-        # else:
-        #     pass
     time.sleep(1)
 
 # print(getwod("http://crossfitbalabanlevent.blogspot.com/2019/10/"))
