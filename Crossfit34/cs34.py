5f2e5ad3146598560ab8acfec3d08482b8d44159
popolinka <55590370+popolinka@users.noreply.github.com>
2020-01-07 15:54:31 +0100

    finalize

--- Crossfit34/cs34.py
+++ Crossfit34/cs34.py
@@ -16,32 +16,27 @@ def getwod(url):
 
 
 wods = []
-aralik = range(1, 7)  # includes 1, but not 3
-for i in aralik:
+for i in range(1, 7):
     url = 'https://crossfit34.com/gunun-antrenmani/page/{}'.format(i) + '/'
     wods.append(getwod(url))
-    # ilk savfayi bitirip ikinci sayfaya gecerken bosluk birakmasi lazim, yoksa son ve ilk idman birlesiyor
-
-    time.sleep(10) # as per /robots.txt
+    time.sleep(10) # to abide /robots.txt
 
 wodsCombinedComa = ""
 i = 0
 while i < len(wods):
     wodsCombinedComa += '?'.join(
-        wods[i])  # text lerde olmayan bir karakter olmasi onemli, yoksa text in kendisni 2 ayri parcaya boluyor
+        wods[i])  # any character that is not present within the wods will work. if not, it will split the wods themselves
     wodsCombinedComa += "?"
     i += 1
 
 wodsCombinedComaSplit = wodsCombinedComa.split('?')
 
-degisecekler = [("ı", "i"), ("Burpees", "Burpee"), ("Dumbbel", "Dumbbell"), ("dumbbells", "Dumbbell"),
+corrections = [("ı", "i"), ("Burpees", "Burpee"), ("Dumbbel", "Dumbbell"), ("dumbbells", "Dumbbell"),
                 ("&", " & "), ("+ ", " + "), ("+", " + "), ("   +  ", " + "), (" )", ")"), ("(", " ("), (" (", " ("),
                 ("Paralel", "Parallel"), ("Russain", "Russian")]
 
-for a, b in degisecekler:
+for a, b in corrections:
     wodsCombinedComaSplit = [s.replace(a, b) for s in wodsCombinedComaSplit]
-
-print(len(wodsCombinedComaSplit))
 
 f = open('CS34_wods.txt', 'w+')
 
