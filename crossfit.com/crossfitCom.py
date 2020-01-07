0364477ea99ad892a95d43e09f59d9d6ed51bca0
popolinka <55590370+popolinka@users.noreply.github.com>
2020-01-07 14:51:19 +0100

    fixed duplicate issue
    a rough, not-so-efficient fix

--- crossfit.com/crossfitCom.py
+++ crossfit.com/crossfitCom.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 workouts = []
 link = "https://www.crossfit.com/workout/?page={}/"
 
+days = ["Monday", "Tuesday" ,"Wednesday" ,"Thursday" ,"Friday" , "Saturday", "Sunday"]
+
 
 def getwod(url):
     url = url
@@ -14,15 +16,15 @@ def getwod(url):
 
     for workout in workoutsTags:
         if workout.get_text() and not (workout.get_text()).startswith("Rest Day"):  # empty and "Rest Day..." check
-            workouts.append(" ".join(workout.get_text().split("\n")).strip())
+            if not workout.get_text().split()[0] in days: # getting rid of day + wod duplicate issue
+                workouts.append(" ".join(workout.get_text().split("\n")).strip())
     return workouts
 
 
-for i in range(1, 3):  # (a,b) where b exclusive
+for i in range(1, 3):  # 5 exclusive
     workouts.append(getwod(link.format(i)).pop(-1))  # pop bc. since return workouts (see getwod()) adds ...
     time.sleep(5)
 
 with open('crossfitCom_wods.txt', 'w+') as f:
     for x in workouts:
         f.write('%s\n' % x)  # splitting each wod/item into a separate row
-# TODO:
